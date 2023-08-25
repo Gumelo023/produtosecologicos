826df6ff7197163f5c2fdfe5ee74852c50ec97e1
Gustavo <gustavomelopereira02@gmail.com>
2023-08-25 12:03:34 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/ecossis/tcc/produtosecologicos/ProdutosecologicosApplication.java
@@ -0,0 +1,13 @@
+package com.ecossis.tcc.produtosecologicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosecologicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosecologicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecossis/tcc/produtosecologicos/ProdutosecologicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecossis.tcc.produtosecologicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosecologicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
